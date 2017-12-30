18213fec74a500d291a4c27314fde13827b4a19f
Mohonri Dorff <mkdorff@users.noreply.github.com>
2017-12-30 09:13:56 -0700

    Create multimap.js

---
+++ multimap.js
@@ -0,0 +1,28 @@
+function multimap() {
+  let arrays = [],
+      res = [],
+      argix = 0,
+      iterator;
+  
+  for (let len = arguments.length, key = 0; key < len; key++, argix++) {
+    if (!Array.isArray(arguments[key])) break; 
+    arrays.push(arguments[key]);
+  }
+  
+  if (arrays.length < 2) {
+    console.error("Use two or more arrays as input");
+    return res;
+  }
+  if (typeof arguments[argix] !== "function") {
+    console.error("Pass the iterator function as the argument following the arrays");
+    return res;
+  }
+  
+  iterator = arguments[argix++];
+  
+  for (let i = 0; i < Math.min.apply(Math, arrays.map(a => a.length)); i++) {
+    res.push(iterator(...arrays.map((a) => (a[i]))))
+  }
+  
+  return res;
+}
